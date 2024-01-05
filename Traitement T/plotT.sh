e6ec3b192a23577bf1226800b8f480c6b3683dad
chrmil <130459909+chrmil@users.noreply.github.com>
2024-01-05 13:36:22 +0100

    Update plotT.sh

--- Traitement T/plotT.sh
+++ Traitement T/plotT.sh
@@ -1,20 +1,21 @@
 #!/usr/local/bin/gnuplot -persist
 gnuplot -persist <<-EOFMarker
-	set terminal pngcairo  transparent enhanced font "arial,10" fontscale 1.0 size 600, 400 
+	set terminal pngcairo  enhanced font "arial,10" fontscale 1.0 size 600, 400 
 	set output 'histograms.png'
 	set boxwidth 0.9 absolute
 	set style fill solid 1.00 border lt -1
-	set key right autotitle columnheader box
+	set key right  noenhanced autotitle columnheader box
 	set style histogram clustered gap 1 
 	unset parametric
 	set datafile missing '-'
 	set style data histograms
 	set xtics nomirror rotate by -45  autojustify 
 	set xtics  norangelimit 
+	set mytics
 	set title "Nombre de trajets et de départs par ville" 
 	set xrange [ * : * ] noreverse writeback
 	set x2range [ * : * ] noreverse writeback
-	set yrange [ * : * ] noreverse writeback
+	set yrange [ 0 : 50 ] noreverse writeback
 	set y2range [ * : * ] noreverse writeback
 	set zrange [ * : * ] noreverse writeback
 	set cbrange [ * : * ] noreverse writeback
