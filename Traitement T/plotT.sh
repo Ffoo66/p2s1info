9a7951a6c3b7c7196a6ab6c9944de78e7e18dbd5
chrmil <130459909+chrmil@users.noreply.github.com>
2024-01-15 15:52:53 +0100

    Update plotT.sh

--- Traitement T/plotT.sh
+++ Traitement T/plotT.sh
@@ -27,5 +27,32 @@ gnuplot -persist <<-EOFMarker
 	plot for [COL=2:3] 'results.dat' using COL:xticlabels(1)
 	pause -1 "\n"
 EOFMarker
-
+gnuplot -persist <<-EOFMarker
+	#set terminal pngcairo  enhanced font "arial,10" fontscale 1.0 size 600, 400 
+	#set output 'histogramsclustered.png'
+	set boxwidth 0.9 absolute
+	set style fill solid 1.00 border lt -1
+	set key right  noenhanced autotitle columnheader box
+	set style histogram clustered gap 1 
+	unset parametric
+	set datafile missing '-'
+	set datafile separator ";"
+	set style data histograms
+	set xtics nomirror rotate by -45  autojustify 
+	set xtics  norangelimit 
+	set mytics
+	set grid xtics mxtics ytics mytics front
+	set title "Nombre de trajets et de départs par ville" 
+	set xrange [ * : * ] noreverse writeback
+	set x2range [ * : * ] noreverse writeback
+	set yrange [ 0 : 50 ] noreverse writeback
+	set y2range [ * : * ] noreverse writeback
+	set zrange [ * : * ] noreverse writeback
+	set cbrange [ * : * ] noreverse writeback
+	set rrange [ * : * ] noreverse writeback
+	set colorbox vertical origin screen 0.9, 0.2 size screen 0.05, 0.6 front  noinvert bdefault
+	NO_ANIMATION = 1
+	plot for [COL=2:3] 'results.dat' using COL:xticlabels(1)
+	pause -1 "\n"
+EOFMarker
 
