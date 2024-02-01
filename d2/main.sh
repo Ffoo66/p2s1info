325368aa798324cd0340c0c30ea11e48631491c8
chrmil <130459909+chrmil@users.noreply.github.com>
2024-02-01 08:24:18 +0100

    Delete d2 directory

--- d2/main.sh
+++
@@ -1,7 +0,0 @@
-LC_NUMERIC="C" awk -F';' '{sum[$6] += sprintf("%f", $5) } END { for (driver in sum) printf "%s %.3f\n",driver, sum[driver] }' data.csv | sort -k3,3nr | head -n 10 >results.data
-# to create a horizontale histogram turn the image by 90 degrees
-gnuplot -e "load 'hst.gnu'"
-# convert is used to create new images while mogrify modifies the existing file
-mogrify -rotate 90 "graph.png"
-
-
