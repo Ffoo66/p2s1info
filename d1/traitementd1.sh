5b3e88a72756589811d3cbf7ca3c89b793246bbd
chrmil <130459909+chrmil@users.noreply.github.com>
2024-02-01 08:24:28 +0100

    Delete d1 directory

--- d1/traitementd1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#grep ";1;" data01.csv  | cut -d";" -f6 | sort | uniq -c | sort -n -r | head -n10 > results.data
-
-awk -F';' '$2 == 1 { count[$6]++ } END { for (driver in count) print driver, count[driver] }' data.csv | sort -k3,3nr | head -n 10 >results.data
-#pour creer un histogramme horizontal on tourne l'histogramme vertical 
-
-gnuplot -e "load 'hst.gnu'"
-mogrify -rotate 90 "histogrammehoriz.png"
